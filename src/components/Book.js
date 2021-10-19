a2d246bf1d960d6fd8877a04ff8436b8539c0de8
Deepakdanger <depk.96.kumar@gmail.com>
2021-10-19 14:23:48 +0530

    Add booklist.js

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,12 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function Books({ books }) {
-  return (
-    <div className="displaybook">
-      {books}
-    </div>
-  );
-}
-
 export default Books;
--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -1,22 +1,28 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import Book from '../components/Book';
 
 function BooksList() {
+
+  const BookList_all = books.map(book => (<Book key = {`book-${book.bookId}`} book = {book} />));
   return (
     <div className="booklist">
-      <div className="book_id">
-        Id
-      </div>
-      <div className="book_name">
-        Name of book
-      </div>
-      <div className="book_author">
-        Author
-      </div>
-      <div className="book_category">
-        category of book
-      </div>
+       <table className = "booklist-table">
+       <thead>
+         <tr className = "booklist-row">
+          <th className = "booklist-header">ID</th>
+          <th className = "booklist-title">Title Of Book</th>
+          <th className = "booklist-category">Category Of Book</th>
+         </tr>
+       </thead>
+       <tbody>
+         {BookList_all}
+       </tbody>
+     </table>
     </div>
   );
 }
 
-export default BooksList;
+const mapStateToProps = (state) => ({ books: state.bookReducer.books });
+
+export default connect(mapStateToProps, null)(BooksList);
