1c115a6f51c76039ea4ece271073be7bf04031dd
Deepakdanger <depk.96.kumar@gmail.com>
2021-10-19 14:24:25 +0530

    Add Book.js and its coment

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,4 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function Books({ books }) {
+    const { bookId, title, category } = books;
+  return (
+    <div className="displaybook">
+      <div className="displaybook_id">
+      {bookId}
+      </div>
+      <div className="displaybook_name">
+      {title}
+      </div>
+      <div className="displaybook_category">
+      {category}
+      </div>
+    </div>
+  );
+}
+
 export default Books;
