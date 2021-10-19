8df56bfdc69cd4eeb74f4960d94937e7c5d291d4
Deepakdanger <depk.96.kumar@gmail.com>
2021-10-19 16:37:29 +0530

    Add action in index.js

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -2,21 +2,21 @@ import React from 'react';
 import Select from 'react-select';
 
 function BooksForm() {
-    const options = [
-        { value: 'action', label: 'Action' },
-        { value: 'biography', label: 'Biography' },
-        { value: 'history', label: 'History' },
-        { value: 'horror', label: 'Horror' },
-        { value: 'kids', label: 'Kids' },
-        { value: 'learning', label: 'Learning' },
-        { value: 'sci-fi', label: 'Sci-Fi' }
-      ]
+  const options = [
+    { value: 'action', label: 'Action' },
+    { value: 'biography', label: 'Biography' },
+    { value: 'history', label: 'History' },
+    { value: 'horror', label: 'Horror' },
+    { value: 'kids', label: 'Kids' },
+    { value: 'learning', label: 'Learning' },
+    { value: 'sci-fi', label: 'Sci-Fi' }
+  ];
   return (
     <div className="booklist">
-        <form>
-      <input type="text" id="title" name="title" placeholder="Title of the book" />
-      <Select options={options} />
-      <button type="submit">Submit</button>
+      <form>
+        <input type="text" id="title" name="title" placeholder="Title of the book" />
+        <Select options={options} />
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
